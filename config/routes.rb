331b4b1bf003aebf0691b4b213dce69cf1d18376
iammikeyoung <myoung10@gmail.com>
2017-10-01 19:54:48 -0400

    signup form and invalid signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'user#create'
   resources :users
 
 end
