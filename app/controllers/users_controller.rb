feed9b90babbfdc9ae394631b684f0497889c39d
iammikeyoung <myoung10@gmail.com>
2017-10-01 20:01:10 -0400

    successful signup path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # do something
+      flash[:success] = "Welcome to Microblogger!"
+      redirect_to @user
     else
       render 'new'
     end
