42c40a808447bd4ae52e62c346c65528ffe74935
iammikeyoung <myoung10@gmail.com>
2017-10-05 13:52:34 -0400

    micropost refinements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token
   has_secure_password
   before_save { self.email = email.downcase }
